1a1d5ceb1f1648940c8661e89f1027507c609454
abraham aranda <a.aranda@cohaerentis.com>
2014-01-04 14:49:51 +0100

    Fluid container added to the footer

--- wp-content/themes/teachnova/templates/footer.php
+++ wp-content/themes/teachnova/templates/footer.php
@@ -1,5 +1,6 @@
 <?php $cols = array('one', 'two', 'three', 'four', 'five'); ?>
-<footer class="content-info container" role="contentinfo">
+<footer class="content-info container-fluid" role="contentinfo">
+<?php /* AAA: -fluid added to de container*/?>
   <div class="row">
     <?php foreach($cols as $col) : if (roots_display_footerbar($col)) : ?>
     <aside class="wp-sidebar <?php echo $col; ?>-footerbar <?php echo roots_sidebar_class($col . '-footer'); ?>" role="complementary">
