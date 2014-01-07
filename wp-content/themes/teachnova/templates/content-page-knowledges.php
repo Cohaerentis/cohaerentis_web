fd7b5f8357735dea84e6a6c620185a057221c3f9
abraham aranda <a.aranda@cohaerentis.com>
2014-01-07 11:47:08 +0100

    List of knowledge areas mocked-up.

--- wp-content/themes/teachnova/templates/content-page-knowledges.php
+++ wp-content/themes/teachnova/templates/content-page-knowledges.php
@@ -27,9 +27,9 @@
 ?>
 <?php if (have_posts()) : the_post(); ?>
     <article <?php post_class() ?> id="page-<?php echo $term->term_id; ?>">
-        <div class="page-header">
+        <!--<div class="page-header">
             <h1 class="entry-title"><?php the_title(); ?></h1>
-        </div>
+        </div>-->
         <div class="entry-content">
             <?php the_content(); ?>
         </div>
--- wp-content/themes/teachnova/templates/element-knowledges-mosaic.php
+++ wp-content/themes/teachnova/templates/element-knowledges-mosaic.php
@@ -1,7 +1,7 @@
 <?php global $knowledges; if (!empty($knowledges)) : ?>
 <div class="tn-knowledges-mosaic row">
    <?php foreach($knowledges as $knowledge) : if (!empty($knowledge->img)) : ?>
-      <div class="col-lg-2 col-md-3 col-sd-4 col-xs-6">
+      <div class="col-lg-2 col-md-3 col-sd-4 col-xs-6 content-knowledges-mosaic">
          <a href="<?php echo $knowledge->url; ?>">
             <?php echo $knowledge->img; ?>
          </a>
