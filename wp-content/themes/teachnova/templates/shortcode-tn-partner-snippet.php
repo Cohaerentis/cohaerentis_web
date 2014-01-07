e3e490fae4d1cdd47594988b15ff6e92f68636a5
abraham aranda <a.aranda@cohaerentis.com>
2014-01-07 09:51:54 +0100

    Partners 2 columns list mocked-up.

--- wp-content/themes/teachnova/templates/shortcode-tn-partner-snippet.php
+++ wp-content/themes/teachnova/templates/shortcode-tn-partner-snippet.php
@@ -17,10 +17,21 @@
         $facebook    = get_post_meta( $partner->ID, 'partner_facebook', true );
         $twitter     = get_post_meta( $partner->ID, 'partner_twitter', true );
         $linkedin    = get_post_meta( $partner->ID, 'partner_linkedin', true );
+        $logo       = get_the_post_thumbnail($partner->ID, 'thumbnail', $attr);
     }
 ?>
 <?php if (!empty($partner)) : ?>
-    TODO : Partner = <a href="<?php echo $url; ?>"><?php echo $name; ?></a>
+<div class="content-partners">
+        <ul>
+            <li><?php echo $logo;?></li>
+        </ul>
+        <ul class="social-icons">
+            <li id="name"><a href="<?php echo $url; ?>"><?php echo $name; ?></a></li>
+            <li><?php echo $position; ?></li>
+            <li class="social"><span style="font-size: 30px;"><a href="#" class="fa fa-globe"><span style="color: transparent; display: none;">icon-globe</span></a></span></li>
+            <li class="social"><span style="font-size: 30px;"><a href="#" class="fa fa-rss-square"><span style="color: transparent; display: none;">icon-rss</span></a></span></li>
+         </ul>
+    </div>
 <?php else : ?>
     TODO : Partner not found for slug = <?php echo $slug; ?>
 <?php endif; ?>
