f08bcfc4daacac06c96d960d5542fe1161223e22
abraham aranda <a.aranda@cohaerentis.com>
2014-01-08 16:42:10 +0100

    Slider for showing our clients.

--- wp-content/themes/teachnova/templates/element-customers-slider.php
+++ wp-content/themes/teachnova/templates/element-customers-slider.php
@@ -1,19 +1,33 @@
-<?php global $customers, $showmax; if (!empty($customers)) : ?>
-<div class="tn-customers-slider">
-   <ul>
-   <?php $n = 0; foreach($customers as $customer) : if (!empty($customer->img)) : ?>
-      <li>
-         <?php if (!empty($customer->url)) : ?>
-         <a href="<?php echo $customer->url; ?>" target="_blank">
-         <?php endif; ?>
-            <?php echo $customer->img; ?>
-         <?php if (!empty($customer->url)) : ?>
-         </a>
-         <?php endif; ?>
-      </li>
-   <?php $n++; if ($n >= $showmax) break; ?>
-   <?php endif; endforeach; ?>
-   </ul>
+<?php global $customers;?>
+<?php if (!empty($customers)) : ?>
+<div id="<?php echo $id; ?>" class="carousel slide media-carousel">
+   <div class="carousel-inner center-block">
+     <?php $i = 0; foreach($customers as $customer) : ?>
+       <?php if (($i % 6) == 0) : ?>
+         <div class="item <?php if ($i == 0) echo 'active'; ?>">
+           <ul class="row text-center">
+       <?php endif; ?>
+             <li class="col-md-2 col-sm-4 col-xs-4">
+               <figure>
+                 <?php echo $customer->img; ?>
+                 <figcaption><?php echo $feature->caption; ?></figcaption>
+               </figure>
+             </li>
+       <?php $i++; if (($i % 6) == 0) : ?>
+           </ul>
+         </div>
+       <?php endif; ?>
+     <?php endforeach; ?>
+     <?php if (($i % 6) != 0) : ?>
+           </ul>
+         </div>
+     <?php endif; ?>
+   </div>
+   <a class="left carousel-control" href="#<?php echo $id; ?>" data-slide="prev">
+     <span class="glyphicon glyphicon-chevron-left"></span>
+   </a>
+   <a class="right carousel-control" href="#<?php echo $id; ?>" data-slide="next">
+     <span class="glyphicon glyphicon-chevron-right"></span>
+   </a>
 </div>
-
 <?php endif; ?>
