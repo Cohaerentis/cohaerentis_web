e1fca2e4f8050d36b07cef5c3b8579e1119e3659
abraham aranda <a.aranda@cohaerentis.com>
2014-01-09 11:35:10 +0100

    Space content mockep-up. Social share element added.

--- wp-content/themes/teachnova/templates/content-space.php
+++ wp-content/themes/teachnova/templates/content-space.php
@@ -22,12 +22,62 @@ ini_set( 'display_errors', 'On' );
     }
 ?>
     <article <?php post_class() ?> id="space-<?php echo $post->ID; ?>">
-        <div class="page-header">
-            <h1 class="entry-title"><?php the_title(); ?></h1>
+        <div class="row space-content col-lg-12">
+          <?php if(!empty($gallery)):?>
+          <div class="slider-gallery col-lg-7">
+            <div id="space-slider-<?php echo $post->ID; ?>" class="carousel slide" data-ride="carousel">
+              <!-- Indicators -->
+              <ol class="carousel-indicators">
+                <?php $i = 0; foreach ($images as $image) : ?>
+                <li data-target="#space-slider-<?php echo $post->ID; ?>"
+                  data-slide-to="<?php echo $i; ?>"
+                  <?php if ($i == 0) : ?>
+                  class="active"
+                <?php endif; ?>
+                >
+              </li>
+              <?php $i++; endforeach; ?>
+            </ol>
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+              <?php $i = 0; foreach ($images as $image) : ?>
+              <?php
+              $src = '/' . $gallery->path . '/' . $image->filename;
+              $class = 'item';
+              if ($i == 0) $class .= ' active';
+              ?>
+              <div class="<?php echo $class; ?>">
+                <img src="<?php echo $src; ?>" alt="<?php echo $image->alttext; ?>" class="img-responsive">
+              </div>
+              <?php $i++; endforeach; ?>
+            </div>
+
+            <!-- Controls -->
+            <a class="left carousel-control" href="#space-slider-<?php echo $post->ID; ?>" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left"></span>
+            </a>
+            <a class="right carousel-control" href="#space-slider-<?php echo $post->ID; ?>" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right"></span>
+            </a>
+          </div>
+          </div>
+        <?php endif; ?>
+          <div class="page-header col-lg-5>">
+              <h1 class="entry-title"><?php the_title(); ?></h1>
+          </div>
+          <div class="entry-description">
+              <?php echo the_excerpt(); ?>
+          </div>
+          <div>
+            <span><a href="<?php echo $url;?>"><strong>Visitar sitio</strong></a></span>
+          </div>
+            <?php get_template_part('templates/element-social-share'); ?>
         </div>
-        <div class="entry-description">
-            <?php echo the_excerpt(); ?>
+        <div class="entry-content">
+            <?php the_content(); ?>
         </div>
+    </article>
+
 <pre>
     url = <?php var_export($url); ?><br>
     gallery path = <?php var_export($gallery->path); ?><br>
@@ -36,62 +86,6 @@ ini_set( 'display_errors', 'On' );
     <img src="<?php echo '/' . $gallery->path . '/' . $image->filename; ?>" class="img-responsive"><br>
     <?php $n++; endforeach; ?>
 </pre>
-        <div class="entry-content">
-            <?php the_content(); ?>
-        </div>
-<!-- Image slider -->
-<div class="row">
-<div class="col-lg-6 col-md-4 col-sd-4 col-xs-12">
-    <div id="space-slider-<?php echo $post->ID; ?>" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-      <?php $i = 0; foreach ($images as $image) : ?>
-        <li data-target="#space-slider-<?php echo $post->ID; ?>"
-            data-slide-to="<?php echo $i; ?>"
-            <?php if ($i == 0) : ?>
-              class="active"
-            <?php endif; ?>
-          >
-        </li>
-      <?php $i++; endforeach; ?>
-      </ol>
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner">
-    <?php $i = 0; foreach ($images as $image) : ?>
-      <?php
-        $src = '/' . $gallery->path . '/' . $image->filename;
-        $class = 'item';
-        if ($i == 0) $class .= ' active';
-      ?>
-        <div class="<?php echo $class; ?>">
-          <img src="<?php echo $src; ?>" alt="<?php echo $image->alttext; ?>" class="img-responsive">
-        </div>
-    <?php $i++; endforeach; ?>
-      </div>
-
-      <!-- Controls -->
-      <a class="left carousel-control" href="#space-slider-<?php echo $post->ID; ?>" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-      </a>
-      <a class="right carousel-control" href="#space-slider-<?php echo $post->ID; ?>" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-      </a>
-    </div>
-</div>
-<div class="col-lg-6 col-md-8 col-sd-8 col-xs-12">
-    <div class="page-header">
-        <h1 class="entry-title"><?php the_title(); ?></h1>
-    </div>
-    <div class="entry-description">
-        <?php the_excerpt(); ?>
-    </div>
-    <div class="entry-link">
-        <a href="<?php echo $url; ?>" target="_blank"><?php echo __('Go to space site'); ?></a>
-    </div>
-</div>
-</div>
-
-    </article>
 <?php else : ?>
     <?php get_template_part('templates/element-if-noresults'); ?>
 <?php endif; ?>
---
+++ wp-content/themes/teachnova/templates/element-social-share.php
@@ -0,0 +1,14 @@
+<div class="social-share">
+	<ul>
+		<li>Compartir en: </li>
+		<li><span class="share" style="font-size: 25px;"><a href="#" class="fa fa-facebook-square"><span style="color: transparent; display: none;">icon-facebook</span></a></span></li>
+		<li><span class="share" style="font-size: 25px;"><a href="#" class="fa fa-linkedin-square"><span style="color: transparent; display: none;">icon-linkedin</span></a></span></li>
+		<li><span class="share" style="font-size: 25px;"><a href="#" class="fa fa-twitter-square"><span style="color: transparent; display: none;">icon-twitter</span></a></span></li>
+		<li><span class="share" style="font-size: 25px;"><a href="#" class="fa fa-google-plus-square"><span style="color: transparent; display: none;">icon-google-plus</span></a></span></li>
+	</ul>
+	<?php if(!empty($qr)):?>
+	<div class="col-lg-12 col-md-12 col-sd-12 col-xs-12">
+		<img src="<?php echo $qr; ?>">
+	</div>
+<?php endif;?>
+</div>
