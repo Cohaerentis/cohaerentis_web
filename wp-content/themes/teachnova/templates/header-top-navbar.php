63e7388c5f6379144d6a159ee83e0572fdd997fd
abraham aranda <a.aranda@cohaerentis.com>
2014-01-14 16:19:50 +0100

    Nav-bar minor changes.

--- wp-content/themes/teachnova/templates/header-top-navbar.php
+++ wp-content/themes/teachnova/templates/header-top-navbar.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><img src='http://www.cohaerentis.teachnova.net/media/logo.png'/></a>
+      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><img src='/media/logo.png'/></a>
     </div>
 
     <nav class="collapse navbar-collapse" role="navigation">
