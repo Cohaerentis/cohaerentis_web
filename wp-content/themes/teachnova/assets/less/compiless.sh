121117a1ea33b7bc9b83f333e2c4ba2e57d1393c
aespinosa <aespinosa@teachnova.com>
2014-01-16 12:17:49 +0100

    Support /usr/local/bin/recess bin path

--- wp-content/themes/teachnova/assets/less/compiless.sh
+++ wp-content/themes/teachnova/assets/less/compiless.sh
@@ -22,6 +22,12 @@ CSS_PATH="$BIN_PATH/../css"
 MAIN_CSS="$CSS_PATH/main.css"
 MAIN_MIN_CSS="$CSS_PATH/main.min.css"
 RECESS_BIN=/usr/bin/recess
+if [ ! -x "$RECESS_BIN" ]; then RECESS_BIN=/usr/local/bin/recess fi
+
+if [ ! -x "$RECESS_BIN" ]; then
+   echo "ERROR : Recess is not globally installed. Please use 'npm install recess -g' to install it"
+   exit 1
+fi
 
 less=`basename "$APP_LESS"`
 css=`basename "$MAIN_CSS"`
