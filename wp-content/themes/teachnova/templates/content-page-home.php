eb4006a61a8fdd5f0bb7ebb45dd37d67f24041c9
abraham aranda <a.aranda@cohaerentis.com>
2014-01-29 18:04:42 +0100

    added hs.

--- wp-content/themes/teachnova/templates/content-page-home.php
+++ wp-content/themes/teachnova/templates/content-page-home.php
@@ -6,7 +6,7 @@
 
 <?php if (have_posts()) : the_post(); ?>
         <div class="row">
-			<div class = "col-lg-6 col-md-6 col-sm-8 col-xs-10 home-content">
+			<div class = "col-lg-6 col-md-6 col-sm-8 col-hs-10 col-xs-10 home-content">
 				<?php the_content(); ?>
 			</div>
         </div>
