4227dfd9ffa20fdd0422b4a339c8677a97c2c77d
aespinosa <aespinosa@teachnova.com>
2014-01-31 22:52:08 +0100

    CSS Bugfixes

--- wp-content/themes/teachnova/templates/footer.php
+++ wp-content/themes/teachnova/templates/footer.php
@@ -1,99 +1,99 @@
 
 <div class="footer">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-6 col-md-6 col-sm-12 col-hs-12 col-xs-12 footer-left ">
-				<div class="row">
-					<div class="col-lg-12 col-md-12 col-sm-12 col-hs-12 col-xs-12 footer-logos ">
-						<div class="col-lg-6 col-md-6 col-sm-6 col-hs-12 col-xs-12">
-							<a href="http://www.opencodex.es" target="_blank"><img class="img-responsive" src="/media/logo-opencodex.png"/><br></a>
-						</div>
-						<div class="col-lg-6 col-md-6 col-sm-6 col-hs-12 col-xs-12">
-							<a href="http://www.teachnova.com" target="_blank"><img class="img-responsive" src="/media/logo-teachnova.png"/><br></a>
-						</div>
-					</div>
-					<div class="col-lg-12 col-md-12 col-sm-12 col-hs-12 col-xs-12 news ">
-						<div class="row">
-							<div class="col-lg-6 col-md-6 col-sm-6 col-hs-6  col-xs-6 news-item">
-								<div class="row">
-									<div class="col-lg-4 col-md-4 col-sm-4 col-hs-4 col-xs-4" col-xs->
-										<img class="img-responsive" src="/media/derecho_200.jpg"/>
-									</div>
-									<div class="col-lg-8 col-md-8 col-sm-8 col-hs-8 col-xs-8" col-xs->
-										<h3 class="news-title">Noticia Uno</h3>
-										<p class="news-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae porttitor quam. Fusce pulvinar,</p>
-									</div>
-								</div>
-							</div>
-							<div class="col-lg-6 col-md-6 col-sm-6 col-hs-6 col-xs-6 news-item">
-								<div class="row">
-									<div class="col-lg-4 col-hs-4  col-xs-col-sm-4 col-hs-4 col-xs-4" col-xs->
-										<img class="img-responsive" src="/media/derecho_200.jpg"/>
-									</div>
-									<div class="col-lg-8 col-hs-8  col-xs-col-sm-8 col-hs-8 col-xs-8" col-xs->
-										<h3 class="news-title">Noticia Dos</h3>
-										<p class="news-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae porttitor quam. Fusce pulvinar,</p>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-6 col-md-6 col-sm-12 col-hs-12 col-xs-12 footer-right">
-				<div class="row">
-					<div class="col-lg-3 col-md-3 col-sm-3 col-hs-6 col-xs-6 blocks">
-						<ul>
-							<li class="up-title">Somos</li>
-							<li><a herf="#">Equipo</a></li>
-							<li><a herf="#">Partners</a></li>
-						</ul>
-						<ul>
-							<li class="up-title">Espacios</li>
-							<li><a herf="#">Plataforma</a></li>
-							<li><a herf="#">Aula</a></li>
-							<li><a herf="#">Consinergia</a></li>
-							<li><a herf="#">Viernes Coherentes</a></li>
-						</ul>
-					</div>
-					<div class="col-lg-4 col-md-4 col-sm-4 col-hs-6 col-xs-6 blocks">
-						<ul>
-							<li class="up-title">Servicios</li>
-							<li><a herf="#">Legales</a></li>
-							<li><a herf="#">Recursos Humanos</a></li>
-							<li><a herf="#">Asesoramiento financiero</a></li>
-							<li><a herf="#">Formación online</a></li>
-							<li><a herf="#">Desarrollo de software</a></li>
-							<li><a herf="#">Internacionalización</a></li>
-							<li><a herf="#">Marketing digital</a></li>
-							<li><a herf="#">Dirección de proyectos</a></li>
-							<li><a herf="#">Inteligencia económica</a></li>
-						</ul>
-					</div>
-					<div class="third col-lg-5 col-md-5 col-sm-5 col-hs-12 col-xs-12 blocks" >
-						<div class="col-lg-12">
-							<i class="fa fa-quote-left"></i>  <span class="quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-							Pellentesque ullamcorper placerat risus, sed porttitor elit.</span>  <i class="fa fa-quote-right" ></i>
-						</div>
-						<div class="col-lg-12 col-md-12 col-sm-12 col-hs-12 col-xs-12">
-							<input placeholder="Buscar..."></input>
-						</div>
-						<div class="col-lg-12 col-md-12 col-sm-12 col-hs-12 col-xs-12 social">
-							<ul>
-								<li><span class="share" style="font-size: 20px;"><a href="#" class="fa fa-facebook"><span style="color: transparent; display: none;">icon-linkedin</span></a></span></li>
-								<li><span class="share" style="font-size: 20px;"><a href="#" class="fa fa-twitter"><span style="color: transparent; display: none;">icon-twitter</span></a></span></li>
-								<li><span class="share" style="font-size: 20px;"><a href="#" class="fa fa-rss"><span style="color: transparent; display: none;">icon-google-plus</span></a></span></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="end-line col-lg-12 col-md-12 col-sm-12 col-hs-12 col-xs-12">
-				<p>Calle Julián Camarillo, 47&nbsp;&nbsp;|&nbsp;&nbsp;Bloque A - Oficina 006&nbsp;&nbsp;|&nbsp;&nbsp;Madrid 28037 - España&nbsp;&nbsp;|&nbsp;&nbsp;(+34) 123 456 789</p>
-				<p>Cohaerentis Consultores, S.L &nbsp;&nbsp;|&nbsp;&nbsp; &copy;2014&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">Aviso Legal</a></p>
-			</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-hs-12 col-xs-12 footer-left ">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-hs-12 col-xs-12 footer-logos ">
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-hs-12 col-xs-12">
+                            <a href="http://www.opencodex.es" target="_blank"><img class="img-responsive" src="/media/logo-opencodex.png"/><br></a>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-hs-12 col-xs-12">
+                            <a href="http://www.teachnova.com" target="_blank"><img class="img-responsive" src="/media/logo-teachnova.png"/><br></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-hs-12 col-xs-12 news ">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-hs-6  col-xs-6 news-item">
+                                <div class="row">
+                                    <div class="col-lg-4 col-md-4 col-sm-4 col-hs-4 col-xs-4">
+                                        <img class="img-responsive" src="/media/derecho_200.jpg"/>
+                                    </div>
+                                    <div class="col-lg-8 col-md-8 col-sm-8 col-hs-8 col-xs-8">
+                                        <h3 class="news-title">Noticia Uno</h3>
+                                        <p class="news-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae porttitor quam. Fusce pulvinar,</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-hs-6 col-xs-6 news-item">
+                                <div class="row">
+                                    <div class="col-lg-4 col-md-4 col-sm-4 col-hs-4 col-xs-4">
+                                        <img class="img-responsive" src="/media/derecho_200.jpg"/>
+                                    </div>
+                                    <div class="col-lg-8 col-md-8 col-sm-8 col-hs-8 col-xs-8">
+                                        <h3 class="news-title">Noticia Dos</h3>
+                                        <p class="news-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae porttitor quam. Fusce pulvinar,</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 col-hs-12 col-xs-12 footer-right">
+                <div class="row">
+                    <div class="col-lg-3 col-md-3 col-sm-3 col-hs-6 col-xs-6 blocks">
+                        <ul>
+                            <li class="up-title">Somos</li>
+                            <li><a herf="#">Equipo</a></li>
+                            <li><a herf="#">Partners</a></li>
+                        </ul>
+                        <ul>
+                            <li class="up-title">Espacios</li>
+                            <li><a herf="#">Plataforma</a></li>
+                            <li><a herf="#">Aula</a></li>
+                            <li><a herf="#">Consinergia</a></li>
+                            <li><a herf="#">Viernes Coherentes</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-hs-6 col-xs-6 blocks">
+                        <ul>
+                            <li class="up-title">Servicios</li>
+                            <li><a herf="#">Legales</a></li>
+                            <li><a herf="#">Recursos Humanos</a></li>
+                            <li><a herf="#">Asesoramiento financiero</a></li>
+                            <li><a herf="#">Formación online</a></li>
+                            <li><a herf="#">Desarrollo de software</a></li>
+                            <li><a herf="#">Internacionalización</a></li>
+                            <li><a herf="#">Marketing digital</a></li>
+                            <li><a herf="#">Dirección de proyectos</a></li>
+                            <li><a herf="#">Inteligencia económica</a></li>
+                        </ul>
+                    </div>
+                    <div class="third col-lg-5 col-md-5 col-sm-5 col-hs-12 col-xs-12 blocks" >
+                        <div class="col-lg-12">
+                            <i class="fa fa-quote-left"></i>  <span class="quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Pellentesque ullamcorper placerat risus, sed porttitor elit.</span>  <i class="fa fa-quote-right" ></i>
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-hs-12 col-xs-12">
+                            <input placeholder="Buscar..."></input>
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-hs-12 col-xs-12 social">
+                            <ul>
+                                <li><span class="share" style="font-size: 20px;"><a href="#" class="fa fa-facebook"><span style="color: transparent; display: none;">icon-linkedin</span></a></span></li>
+                                <li><span class="share" style="font-size: 20px;"><a href="#" class="fa fa-twitter"><span style="color: transparent; display: none;">icon-twitter</span></a></span></li>
+                                <li><span class="share" style="font-size: 20px;"><a href="#" class="fa fa-rss"><span style="color: transparent; display: none;">icon-google-plus</span></a></span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="end-line col-lg-12 col-md-12 col-sm-12 col-hs-12 col-xs-12">
+                <p>Calle Julián Camarillo, 47&nbsp;&nbsp;|&nbsp;&nbsp;Bloque A - Oficina 006&nbsp;&nbsp;|&nbsp;&nbsp;Madrid 28037 - España&nbsp;&nbsp;|&nbsp;&nbsp;(+34) 123 456 789</p>
+                <p>Cohaerentis Consultores, S.L &nbsp;&nbsp;|&nbsp;&nbsp; &copy;2014&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">Aviso Legal</a></p>
+            </div>
+        </div>
+    </div>
 </div>
