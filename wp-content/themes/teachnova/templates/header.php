d3a191dee03bbc32573ff5f93c99305efe7365b0
aespinosa <aespinosa@teachnova.com>
2014-02-06 20:31:51 +0100

    CSS Bugfixes

--- wp-content/themes/teachnova/templates/header.php
+++ wp-content/themes/teachnova/templates/header.php
@@ -10,7 +10,7 @@
         </button>
         <a class="navbar-brand" href="<?php echo home_url(); ?>/"></a>
       </div>
-      <nav class="collapse navbar-collapse in" role="navigation">
+      <nav class="collapse navbar-collapse" role="navigation">
         <?php
           if (has_nav_menu('primary_navigation')) :
             wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
