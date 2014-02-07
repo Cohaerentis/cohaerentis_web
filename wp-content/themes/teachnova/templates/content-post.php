5174201433251c53dae6596caa4549a63782e640
abraham aranda <a.aranda@cohaerentis.com>
2014-02-07 12:05:43 +0100

    background added.

--- wp-content/themes/teachnova/templates/content-post.php
+++ wp-content/themes/teachnova/templates/content-post.php
@@ -4,24 +4,35 @@
     File: content-post
 </pre>
 <?php if (have_posts()) : the_post(); ?>
-    <article <?php post_class() ?> id="page-<?php echo $term->term_id; ?>">
-        <div class="page-header post-title">
-            <h1 class="entry-title"><?php the_title(); ?></h1>
-        </div>
-        <div class="row post-header">
-            <?php echo the_post_thumbnail('large'); ?>
-            <?php echo the_date('d F Y');?> - <?php echo the_tags('#',', #','.');?>
-        </div>
-        <div class="entry-content post-content">
-            <?php the_content(); ?>
-        </div>
-        <div class="row">
+<article <?php post_class() ?> id="page-<?php echo $term->term_id; ?>">
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-hs-12 col-xs-12 single-post">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-hs-12 col-xs-12 post-title">
+                    <h2 class="h2"><?php the_title(); ?></h2>
+                </div>
+                <div class="news-marker"></div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-hs-12 col-xs-12 post-img">
+                    <?php echo the_post_thumbnail('large'); ?>
+                </div>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-hs-12 col-xs-12 post-date">
+                    <?php echo the_date('d F Y');?> - <?php echo the_tags('#',', #','.');?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-hs-12 col-xs-12 post-content">
+                    <?php the_content(); ?>
+                </div>
+            </div>
             <?php get_template_part('templates/element-social-share');?>
+            <div class="row single-post-comments">
+                <?php comments_template('comments.php'); ?>
+            </div>
         </div>
-        <div class="row single-post-comments"
-            <?php comments_template('comments.php'); ?>
-        </div>
-    </article>
+    </div>
+</article>
 <?php else : ?>
     <?php get_template_part('templates/element-if-noresults'); ?>
 <?php endif; ?>
