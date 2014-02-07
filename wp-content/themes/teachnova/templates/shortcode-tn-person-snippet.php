26cdb188fd6dc1ab69da51495bca0d836b473432
aespinosa <aespinosa@teachnova.com>
2014-02-07 12:05:59 +0100

    CSS bugfix in team an person

--- wp-content/themes/teachnova/templates/shortcode-tn-person-snippet.php
+++ wp-content/themes/teachnova/templates/shortcode-tn-person-snippet.php
@@ -29,9 +29,9 @@
         <div class="col-lg-4 col-md-4 col-sm-4 col-hs-4 col-xs-12 person-img">
             <a href="<?php echo $url;?>"><?php echo $photo; ?></a>
         </div>
-        <div class="col-lg-8 col-md-8 col-sm-8 col-hs-8 col-xs-12 person-info">
-            <div class="row">
-                <div class="info-marker"></div>
+        <div class="col-lg-8 col-md-8 col-sm-8 col-hs-8 col-xs-12">
+            <div class="row person-info">
+                <?php /* <div class="info-marker"></div> */ ?>
                 <div class="col-lg-12 col-md-12 col-sm-12 col-hs-12 col-xs-12 person-info-name">
                     <span class="name"><a href="<?php echo $url; ?>"><?php echo $name; ?></a></span>
                     <p><?php echo $position; ?></p>
