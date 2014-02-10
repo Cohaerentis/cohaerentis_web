ef7236fd60035043c16d5b1e4ae829db745eb4a9
abraham aranda <a.aranda@cohaerentis.com>
2014-02-10 21:05:53 +0100

    Little bugs fixed.

--- wp-content/themes/teachnova/templates/comments.php
+++ wp-content/themes/teachnova/templates/comments.php
@@ -49,7 +49,7 @@
     <?php else : ?>
       <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
         <?php if (is_user_logged_in()) : ?>
-          <p>
+          <p class="comment-logged-in">
             <?php printf(__('Logged in as <a href="%s/wp-admin/profile.php">%s</a>.', 'roots'), get_option('siteurl'), $user_identity); ?>
             <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="<?php __('Log out of this account', 'roots'); ?>"><?php _e('Log out &raquo;', 'roots'); ?></a>
           </p>
